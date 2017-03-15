f22a68f923c0be16737f967b7bba3187846649ba
Manjunadh Veeravalli <P7111009@NESSINBLR.COM>
2017-03-15 12:06:06 +0530

    price service with db with cache

--- src/main/java/se/callista/microservices/core/product/controllers/PriceController.java
+++ src/main/java/se/callista/microservices/core/product/controllers/PriceController.java
@@ -2,6 +2,8 @@ package se.callista.microservices.core.product.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,22 +14,25 @@ import se.callista.microservices.core.product.service.PriceService;
 
 @RestController
 public class PriceController {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(PriceController.class);
 	@Autowired
 	private PriceService priceService;
 
 	@RequestMapping("/price")
 	public List<Price> getPrice() {
+		LOGGER.info("get all prices controller");
 		return priceService.getAllPrices();
 	}
 
 	@RequestMapping("/price/{id}")
 	public Price getPriceById(@PathVariable String id) {
+		LOGGER.info("get price for id" + id);
 		return priceService.getPriceById(id);
 	}
 
 	@RequestMapping("/price/geyBysku/{sku}")
 	public List<Price> getPriceByName(@PathVariable String skuId) {
+		LOGGER.info("get price for sku id" + skuId);
 		return priceService.getPriceBySku(skuId);
 	}
 
--- src/main/java/se/callista/microservices/core/product/service/PriceService.java
+++ src/main/java/se/callista/microservices/core/product/service/PriceService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,17 +17,17 @@ import se.callista.microservices.core.product.model.Price;
 @Transactional
 public class PriceService {
 	private static final Logger LOGGER = LoggerFactory.getLogger(PriceService.class);
-
+	public static final String priceName = "created";
 	@Autowired
 	private PriceRepository priceRepository;
-
+	@Cacheable(value = "prices")
 	public List<Price> getAllPrices() {
 		LOGGER.info("fetching all prices");
 		List<Price> products = new ArrayList<>();
 		priceRepository.findAll().forEach(products::add);
 		return products;
 	}
-
+	@Cacheable(value = "prices", key = "#id")
 	public Price getPriceById(String id) {
 		LOGGER.info("fetching price for id" + id);
 		return priceRepository.findOne(id);
